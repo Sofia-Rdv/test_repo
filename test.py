7d9761741660f24e8dc64729befbe106b427e159
Sofia-Rdv <keller8590g@gmail.com>
2025-06-12 18:04:54 +0300

    Изменена функция приветствия, теперь там кроме приветствия есть вопрос о том, сколько лет пользователю. Добавлена функция ответа на вопрос о возрасте.

--- test.py
+++ test.py
@@ -3,8 +3,14 @@
 
 # функция приветствия
 def welcome(name):
-    print(f'Hello, {name}')
+    print(f'Hello, {name}. How old are you?')
+
+
+# функция ответа с возрастом
+def answer_age(age):
+    print(f'Hello, I am {age} years old.')
 
 
 if __name__ == '__main__':
     welcome('Sonya')
+    answer_age(25)
